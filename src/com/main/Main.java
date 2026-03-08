00f78d47ab207f90ea0e66d89ad2e180d6fdc082
merock2024 <merock2024@gmail.com>
2026-03-08 15:41:11 +0530

    [Abhilaksh] Coded the UC5 that provides Add-on service selection.

--- src/com/main/Main.java
+++ src/com/main/Main.java
@@ -8,7 +8,7 @@ import com.inventory.Inventory;
 /*
  * 
  * @author: Abhilaksh
- * @version: UC4
+ * @version: UC5
  * 
  * 
  * */
@@ -18,11 +18,12 @@ public class Main {
 	public static HashMap<String,Inventory> map = new HashMap<>();
 	public static HashSet<String> bookedRooms = new HashSet<>();
 	public static HashMap<String,HashSet<String>> assignedRooms = new HashMap<>();
+	public static HashMap<String,ArrayList<String>> Services = new HashMap<>();
 
 	static {
-		map.put("Single",new Inventory(2000,5));
-		map.put("Double",new Inventory(3000,3));
-		map.put("Suite",new Inventory(4000,2));
+		map.put("Single",new Inventory(20000,5));
+		map.put("Double",new Inventory(30000,3));
+		map.put("Suite",new Inventory(50000,2));
 		
 		assignedRooms.put("Single", new HashSet<>());
 		assignedRooms.put("Double", new HashSet<>());
@@ -52,6 +53,17 @@ public class Main {
 			else if(x==1) {
 				if(found) {
 					BookingRequest.booking(roomType,user);
+					System.out.println("If Additional Services Required ? type Yes");
+					String serviceInput = sc.nextLine();
+					if(serviceInput.equals("Yes")) {
+						System.out.println("Breakfast//Spa//Pickup");
+						String services = sc.nextLine();
+						String[] arr = services.split(" ");
+						Services.put(roomType+(map.get(roomType).roomCount+1), new ArrayList<>());
+						for(String sr : arr) {
+							Services.get(roomType+(map.get(roomType).roomCount+1)).add(sr);
+						}
+					}
 				}
 			}
 			else if(x==2) {
@@ -63,6 +75,7 @@ public class Main {
 		
 		System.out.println("Booked rooms list: "+ bookedRooms);
 		System.out.println(assignedRooms);
+		System.out.println(Services);
 		
 		sc.close();
 	}
